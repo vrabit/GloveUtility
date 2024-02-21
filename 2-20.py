e01300a37b86ac51a54c8223ef8a742cbd652d32
michaelhiggins8 <150331335+michaelhiggins8@users.noreply.github.com>
2024-02-20 21:14:14 -0800

    Update 2-20.py

--- 2-20.py
+++ 2-20.py
@@ -1,57 +1,72 @@
-from controller import Robot, Keyboard
+from controller import Robot,Keyboard
 
-# Initialize the robot and timestep
 robot = Robot()
 timestep = int(robot.getBasicTimeStep())
 
-# Initialize motors
-m1_motor = robot.getDevice("m1_motor")
-m1_motor.setPosition(float('inf'))
-m1_motor.setVelocity(0)
-m2_motor = robot.getDevice("m2_motor")
-m2_motor.setPosition(float('inf'))
-m2_motor.setVelocity(0)
-m3_motor = robot.getDevice("m3_motor")
-m3_motor.setPosition(float('inf'))
-m3_motor.setVelocity(0)
-m4_motor = robot.getDevice("m4_motor")
-m4_motor.setPosition(float('inf'))
-m4_motor.setVelocity(0)
 
-# Initialize keyboard
+motor1 = robot.getDevice("m1_motor")  
+motor2 = robot.getDevice("m2_motor")  
+motor3 = robot.getDevice("m3_motor")  
+motor4 = robot.getDevice("m4_motor")  
+
 keyboard = Keyboard()
 keyboard.enable(timestep)
 
-# Define the step size for velocity change
-step_size = 1.0
 
-# Set initial velocities
-    vx = 0
-    vy = 0
-    vz = 0
-    yaw_rate = 0
+motor1.setPosition(float('inf'))
+motor2.setPosition(float('inf'))
+motor3.setPosition(float('inf'))
+motor4.setPosition(float('inf'))
 
-# Main control loop
+
+velocity = 1
+motor1.setVelocity(velocity)
+motor2.setVelocity(velocity)
+motor3.setVelocity(velocity)
+motor4.setVelocity(velocity)
+
+holder = 55.4
+second = 0
+forward = 0.001
 while robot.step(timestep) != -1:
     key = keyboard.getKey()
-
+    second +=1
+    if second > 75:
+        if ((second // 10)) % 2 != 0:
+           holder = 55.3359
+        else:
+           holder = 55.4
+    holder1 = -(holder)
+    holder2= holder
+    holder3 = -(holder)
+    holder4 = holder
     
-    # Interpret keyboard commands
     if key == Keyboard.UP:
-        vx += step_size  # Increase forward velocity
+        holder1 = holder1 + forward
+        holder2 = holder2 - forward
+        holder3 = holder3 - forward
+        holder4 = holder4 + forward
     elif key == Keyboard.DOWN:
-        vx -= step_size  # Decrease forward velocity
-    elif key == Keyboard.RIGHT:
-        yaw_rate -= step_size  # Increase clockwise yaw rate
+        holder1 = holder1 - forward
+        holder2 = holder2 + forward
+        holder3 = holder3 + forward
+        holder4 = holder4 - forward
     elif key == Keyboard.LEFT:
-        yaw_rate += step_size  # Increase counterclockwise yaw rate
-    elif key == ord('W'):
-        vz += step_size  # Increase ascending velocity
-    elif key == ord('S'):
-        vz -= step_size  # Increase descending velocity
-
-    # Set motor velocities based on keyboard commands
-    m1_motor.setVelocity(vx - vy - yaw_rate + vz)
-    m2_motor.setVelocity((vx + vy + yaw_rate + vz)*-1)
-    m3_motor.setVelocity(-vx + vy - yaw_rate + vz)
-    m4_motor.setVelocity((-vx - vy + yaw_rate + vz)*-1)
+        holder1 = holder1 + forward
+        holder2 = holder2 + forward
+        holder3 = holder3 - forward
+        holder4 = holder4 - forward
+    elif key == Keyboard.RIGHT:
+        holder1 = holder1 - forward
+        holder2 = holder2 - forward
+        holder3 = holder3 + forward
+        holder4 = holder4 + forward
+    
+    
+    
+    
+    motor1.setVelocity(holder1)
+    motor2.setVelocity(holder2)
+    motor3.setVelocity(holder3)
+    motor4.setVelocity(holder4)
+        
